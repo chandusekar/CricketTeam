a05400913db961ef3c0f75141e158a31ca3bf2d1
chandusekar <chandusekar8222@gmail.com>
2023-08-14 06:16:28 +0000

    cricket team solution

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ const app = express();
 app.use(express.json());
 let database = null;
 
-const initializeDBAndServer = async () => {
-  try {
-    database = await open({
-      filename: databasePath,
-      driver: sqlite3.Database,
-    });
-    app.listen(3000, () => {
-      console.log("Server Running at http://localhost:3000/");
-    });
-  } catch (error) {
-    console.log(`DB Error: ${error.message}`);
-    process.exit(1);
-  }
+const initializeDbAndServer = async () =>{
+    try{
+        database = await open({
+            filename: databasePath,
+            driver : sqlite3.Database,
+        });
+    app.listen(3000,() =>
+    console.log("Server Running at http://localhost:3000/")
+    );    
+    }catch (error) {
+        console.log(`DB Error: ${error.message}`);
+        process.exit(1);
+    }
 };
 
-initializeDBAndServer();
+initializeDbAndServer();
 
 const convertDbObjectToResponseObject = (dbObject) => {
   return {
@@ -47,16 +47,16 @@ app.get("/players/", async (request, response) => {
   );
 });
 
-app.get("/players/:playerId/", (request, response) => {
+app.get("/players/:playerId/", async (request, response) => {
   const { playerId } = request.params;
-  const getPlayersQuery = `
+  const getPlayerQuery = `
     SELECT
     *
     FROM
     cricket_team
     WHERE
     player_id=${playerId}`;
-  const player = await.database.get(getPlayersQuery);
+  const player = await.database.get(getPlayerQuery);
   response.send(convertDbObjectToResponseObject(player));
 });
 
@@ -66,7 +66,7 @@ app.post("/players/", async (request, response) => {
     INSERT INTO
     cricket_team(player_name,jersey_number,role)
     VALUES
-    ('${playerName}','${jerseyNumber}','${role}');`;
+    ('${playerName}',${jerseyNumber},'${role}');`;
   const player = await database.run(postPlayerQuery);
   response.send("Player Added to Team");
 });
@@ -79,8 +79,8 @@ app.put("/players/:playerId/", async (request, response) => {
     cricket_team
     SET
     player_name = '${playerName}',
-    jersey_number ='${jerseyNumber},
-    role = ${role}
+    jersey_number = ${jerseyNumber},
+    role = '${role}'
     WHERE
     player_id ='${playerId}'`;
 
